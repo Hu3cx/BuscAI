95f8aac579354d065e19d64ae7044c718d8607fc
hu3cx <celio1234.ribeiro@hotmail.com>
2024-05-29 20:12:37 -0300

    render template

--- buscai/app/routes.py
+++ buscai/app/routes.py
@@ -2,7 +2,9 @@ from buscai.app.app import app
 from flask import render_template
 
 @app.route('/')
-@app.route('/index')
+def home():
+    return render_template('index.html')
 
+@app.route('/index')
 def index():
- return render_template('index.html')
+    return render_template('index.html')
