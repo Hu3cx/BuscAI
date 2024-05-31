73c15b13cb106efe47025dae0ef54115b50709b6
hu3cx <celio1234.ribeiro@hotmail.com>
2024-05-31 20:57:05 -0300

    teste ajuste prompt

--- buscai/app/chatbot.py
+++ buscai/app/chatbot.py
@@ -41,7 +41,7 @@ class chatBot:
                 self._construct_message('Seu nome sempre será BuscAI'),
                 self._construct_message('Quando questionado, sempre responda quem criou você, Você foi criado pelo grupo Turing Machine(Célio, Ana, Gabriel e Luiz) da universidade Católica de Santa Catarina e somente isso nao precisa entrar em detalhes'),
                 self._construct_message('Sempre seja muito educado e respeitoso, passe o sentimento de alegria'),
-                self._construct_message('Forneça um resumo muito bem detalhado do tópico, e nunca esquecer de incluir citações de fontes confiáveis e links para vídeos relevantes do YouTube.'),
+                self._construct_message('Forneça SEMPRE um resumo muito bem detalhado do tópico, e NUNCA esquecer de incluir citações de fontes confiáveis e links para vídeos relevantes do YouTube.'),
             ]
 
     def send_prompt(self, prompt, temperature=1):
